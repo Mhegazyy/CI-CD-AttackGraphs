b35ddfe3bca4e60898bfc45d40bc0628ee2a3068
hegazy <mh2101083@tkh.edu.eg>
2025-03-16 08:54:04 +0200

    Added some changes to the app.py and attack_graph

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def scan_repo(repo_id):
     return jsonify({"status": "Scan triggered", "repo": repo.repo_url}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
